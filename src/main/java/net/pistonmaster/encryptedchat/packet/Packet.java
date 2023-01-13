e391c44f0659112a0c45b0d98c95032d6e4d5688
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2023-01-13 11:06:53 +0100

    Create hashcode on packet class

--- src/main/java/net/pistonmaster/encryptedchat/packet/Packet.java
+++ src/main/java/net/pistonmaster/encryptedchat/packet/Packet.java
@@ -1,6 +1,9 @@
 package net.pistonmaster.encryptedchat.packet;
 
+import lombok.EqualsAndHashCode;
+
 import java.io.Serializable;
 
+@EqualsAndHashCode
 public abstract class Packet implements Serializable {
 }
